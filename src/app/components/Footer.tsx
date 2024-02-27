859c17e820a758fcdd14d28146db22519f774944
Victoria <victoriacatshirley@gmail.com>
2024-02-26 21:04:01 -0300

    Arrumando a responsividade do footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,76 +1,110 @@
 import React from "react";
 
 export default function Footer() {
-  return(
+  return (
     <>
-    
-    <div className="flex w-340 lg:h-96 md:h-80 bg-purple-dark relative">
-        <div className="h-72  lg:pr-24 md:px-8 lg:py-16 md:px-8">
-            <p className="lg:text-5xl md:text-2xl font-bebas text-white">DESENVOLVIDO POR:</p>
+      <footer className="font-poppins text-justify px-9 py-12 md:px-20 md:py-14 text-sm md:text-base lg:px-56 bg-purple-dark text-white">
+        <div className="mb-6 md:mb-12">
+          <p className="font-bebas text-xl md:text-3xl text-white">
+            DESENVOLVIDO POR:
+          </p>
         </div>
-        <div id="nomes" className="flex lg:gap-8 md:gap-4 lg:mr-4 md:mr-4 lg:mt-36 md:mt-28">
-        <div className="flex-row h-12  flex flex-col items-center" id="ana">
-                <div className="">
-                    <p className="font-poppins text-white lg:text-xl md:text-lg h-12 text-center">Ana Clara</p>
-                </div>
 
-                    <div id="redes-sociais" className="flex items-center justify-center h-20  ">
+        <div className="flex gap-9 md:gap-24 justify-center">
+          <div>
+            <p className="text-white md:text-lg lg:text-xl text-center">
+              Ana Clara
+            </p>
+            
 
-                    <div className="flex flex-row"> 
-                        <a target="blank" href="https://github.com/anacsalves">
-                            <img className="lg:h-16 md:10" src="./icon/icon-github.png" alt="imagem da logo do github" />
-                        </a>
-                    </div>
-                    <div className="flex flex-row"> 
-                        <a target="blank" href="mailto:anacsalves@unifei.edu.br">
-                            <img className="lg:h-14 md:10" src="./icon/icon-email.png.png" alt="imagem de um desenho de uma carta" />
-                        </a>
-                    </div>
-                    <div className="flex flex-row">
-                         <a target="blank" href="https://www.instagram.com/anaclarasalves">
-                            <img className="lg:h-16 md:10" src="./icon/icon-instagram.png" alt="imagem da logo do instagram" />
-                        </a>
-                    </div>
-
-                    </div>                          
+            <div className="flex items-center justify-center h-20">
+              <div className="flex flex-row">
+                <a target="blank" href="https://github.com/anacsalves">
+                  <img
+                    className="h-10 md:h-16"
+                    src="./icon/icon-github.png"
+                    alt="Imagem da logo do GitHub"
+                  />
+                </a>
+              </div>
+              <div className="flex flex-row">
+                <a target="blank" href="mailto:anacsalves@unifei.edu.br">
+                  <img
+                    className="h-9 md:h-14"
+                    src="./icon/icon-email.png.png"
+                    alt="Imagem de um desenho de uma carta"
+                  />
+                </a>
+              </div>
+              <div className="flex flex-row">
+                <a
+                  target="blank"
+                  href="https://www.instagram.com/anaclarasalves"
+                >
+                  <img
+                    className=" h-10 md:h-16"
+                    src="./icon/icon-instagram.png"
+                    alt="Imagem da logo do Instagram"
+                  />
+                </a>
+              </div>
             </div>
-                          
-            <div className="flex-row h-12  flex flex-col items-center" id="vic">
-                <div className="">
-                    <p className="font-poppins text-white lg:text-xl md:text-lg h-12 text-center">Victoria Shirley</p>
+          </div>
+
+          <div>
+            <div>
+              <p className="text-white md:text-lg lg:text-xl text-center">
+                Victória Shirley
+              </p>
+
+              <div className="flex items-center justify-center h-20">
+                <div className="flex flex-row">
+                  <a target="blank" href="https://github.com/victoriashirley">
+                    <img
+                      className="h-10 md:h-16"
+                      src="./icon/icon-github.png"
+                      alt="Imagem da logo do GitHub"
+                    />
+                  </a>
                 </div>
-
-                    <div id="redes-sociais" className="flex items-center justify-center h-20  ">
-
-                    <div className="flex flex-row"> 
-                        <a target="blank" href="https://github.com/victoriashirley">
-                            <img className="lg:h-16 md:10" src="./icon/icon-github.png" alt="imagem da logo do github" />
-                        </a> 
-                    </div>
-                    <div className="flex flex-row"> 
-                        <a target="blank" href="mailto:victoriacatshirley@gmail.com">
-                            <img className="lg:h-14 md:10" src="./icon/icon-email.png.png" alt="desenho deuma cartas" />
-                        </a> 
-                    </div>
-                    <div className="flex flex-row"> 
-                        <a target="blank" href="https://www.instagram.com/viihsilva0312">
-                            <img className="lg:h-16 md:10" src="./icon/icon-instagram.png" alt="imagem da logo do instagram" />
-                        </a> 
-                    </div>
-
-                    </div>         
+                <div className="flex flex-row">
+                  <a target="blank" href="mailto:victoriacatshirley@gmail.com">
+                    <img
+                      className="h-9 md:h-14"
+                      src="./icon/icon-email.png.png"
+                      alt="Imagem de um desenho de uma carta"
+                    />
+                  </a>
+                </div>
+                <div className="flex flex-row">
+                  <a
+                    target="blank"
+                    href="https://www.instagram.com/viihsilva0312"
+                  >
+                    <img
+                      className=" h-10 md:h-16"
+                      src="./icon/icon-instagram.png"
+                      alt="Imagem da logo do Instagram"
+                    />
+                  </a>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
 
-        <div className="flex absolute bottom-0 right-0 lg:mb-12 md:mb-6 lg:mr-24 md:mr-5" >
-            <a target="blank" href="https://www.byronsolutions.com/">
-        <picture className="" >
-            <img className="h-12 px-240 py-88 " src="./img/logo-byron.png" alt="imagem da logo da byron.solutions" />
-        </picture>
-        </a>
+        <div className="flex flex-col items-end mt-12 md:mt-16">
+          <a target="blank" href="https://www.byronsolutions.com/">
+            <picture className="">
+              <img
+                className="w-48 md:w-64"
+                src="./img/logo-byron.png"
+                alt="imagem da logo da byron.solutions"
+              />
+            </picture>
+          </a>
         </div>
-    </div>
-    
+      </footer>
     </>
-  )
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
